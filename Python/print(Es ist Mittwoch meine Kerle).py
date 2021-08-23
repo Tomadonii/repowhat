f8d726dda40a4ee7531ff4a6c91108d192130c97
Pascal Mausch <pascal.mausch@gmail.com>
2021-08-23 13:55:45 +0200

    Nur ne neue Datei

---
+++ Python/print(Es ist Mittwoch meine Kerle).py
@@ -0,0 +1,7 @@
+friends = ["Kevin", "Mike", "Pascal", "marc", "Ruben"]
+numbers = [1, 2, 3, 4]
+friends.extend(numbers)
+friends.pop()
+friends.append("Joe")
+friends.insert(1, "Craige")
+print(friends)
